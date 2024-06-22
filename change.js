866ab3f575294de798022f192eadb7c7cd885d97
eshaalal <eshalal888@gmail.com>
2024-06-22 14:03:38 +0530

    added

--- change.js
+++ change.js
@@ -4,7 +4,7 @@ form.addEventListener('submit', function(e){
     const height=parseInt(document.querySelector('#height').value);
     const weight=parseInt(document.querySelector('#weight').value);
     const results=document.querySelector('#results');
-
+    const guide=document.querySelectorAll('#weight-guide p');
     if(height==='' || height<0|| isNaN(height)){
         results.innerHTML=`Please give a valid height ${height}`;
     }else if(weight==='' || weight<0|| isNaN(weight)){
@@ -13,6 +13,14 @@ form.addEventListener('submit', function(e){
     }else{
         const bmi=(weight/((height*height)/10000)).toFixed(2);
         results.innerHTML=`<span>${bmi}</span>`;
+        if (bmi < 18.6) {
+            guide[0].style.color = 'red';
+        } else if (bmi >= 18.6 && bmi <= 24.9) {
+            guide[1].style.color = 'green';
+        } else if (bmi > 24.9) {
+            guide[2].style.color = 'orange';
+        }
     }
+    
 
 });
